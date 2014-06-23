c9d558ee1d0c274ba84104de66be7bcae0e7637a
trestan <trestan@trestan-ThinkPad-W530.(none)>
2014-06-23 15:03:07 +0200

    -

---
+++ gi2name.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while getopts ":g:h" opt; do
+  case $opt in
+    g)
+      echo "-g was triggered, Parameter: $OPTARG" >&2
+      gi=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+    h)
+      echo -e "utilisation:\ngi2name.sh -g [gi number]"
+      exit 1
+  esac
+done
+
+taxdb_dir="/home/trestan/Dropbox/taxdb"
+
+taxid=$(grep "^$gi" $taxdb_dir/gi_taxid_prot.dmp | cut -f2)
+name=$(grep -w "^$taxid" $taxdb_dir/taxdump/names.dmp | grep "scientific name" | cut -d "|" -f 2 | sed 's/\t//')
+echo $name
