cbf8b545713f4b85fbc30e19d9aa6aab8b86cd26
Trestan Pillonel <trestan.pillonel@gmail.com>
2015-03-13 17:04:32 +0100

    refactoring

--- svg2pdf.sh
+++ svg2pdf.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# convert svg 2 pdf using inkscape
+
+# Author: Trestan Pillonel (trestan.pillonel[]gmail.com)
+# Date: 02.2015
+# -----------------------------------------------------
+
 for i in $@;do
 	inkscape -z --export-pdf="$(basename $i .svg).pdf" $i
 done
