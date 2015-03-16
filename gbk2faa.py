3ca0e902a728b7ec4e5976bde4867a24336731cb
Trestan Pillonel <trestan.pillonel@gmail.com>
2015-03-16 15:45:12 +0100

    new header formatting option

--- gbk2faa.py
+++ gbk2faa.py
@@ -10,11 +10,21 @@
 # ---------------------------------------------------------------------------
 
 
-def gbk2faa(seq_records, outname):
-    import sys
+def gbk2faa(seq_records, outname, format = False):
+    import re
     output_handle = open(outname, "w")
 
     for record in seq_records:
+        description = record.description
+        description = re.sub(", complete genome\.", "", description)
+        description = re.sub(", complete sequence\.", "", description)
+        description = re.sub("strain ", "", description)
+        description = re.sub("str\. ", "", description)
+        description = re.sub(" complete genome sequence\.", "", description)
+        description = re.sub(" complete genome\.", "", description)
+        description = re.sub(" chromosome", "", description)
+        description = re.sub(" DNA", "S.", description)
+
         for seq_feature in record.features:
             if seq_feature.type == "CDS":
                 # check presence of a protein sequence
@@ -27,29 +37,40 @@ def gbk2faa(seq_records, outname):
                     continue
                 #assert len(seq_feature.qualifiers['translation'])==1
                 # gi|83716028|ref|YP_443839.1| matrix protein [Avian metapneumovirus]
-                try:
-                    output_handle.write(">gi|%s|ref|%s| %s [%s]\n%s\n" % (
-                            seq_feature.qualifiers["db_xref"][0].split(":")[1],
-                            seq_feature.qualifiers["protein_id"][0],
-                            seq_feature.qualifiers["product"][0],
-                            record.description,
-                            seq_feature.qualifiers['translation'][0]))
-                except:
+                if format:
+                    # output only locus tag and species name
                     try:
-                    
-                        output_handle.write(">gi|%s|ref|%s| [%s]\n%s\n" % (
-                            seq_feature.qualifiers["db_xref"][0].split(":")[1],
-                            seq_feature.qualifiers["protein_id"][0],
-                            #seq_feature.qualifiers["note"][0],
-                            record.description,
-                            seq_feature.qualifiers['translation'][0]))
+                        output_handle.write(">%s %s\n%s\n" % (
+                                seq_feature.qualifiers["protein_id"][0],
+                                                description,
+                                                seq_feature.qualifiers['translation'][0])
                     except:
-                        output_handle.write(">gi|%s|ref|%s| [%s]\n%s\n" % (
-                            seq_feature.qualifiers["locus_tag"][0],
-                            seq_feature.qualifiers["locus_tag"][0],
-                            #seq_feature.qualifiers["note"][0],
-                            record.description,
-                            seq_feature.qualifiers['translation'][0]))                    
+                        print seq_feature
+                else:
+
+                    try:
+                        output_handle.write(">gi|%s|ref|%s| %s [%s]\n%s\n" % (
+                                seq_feature.qualifiers["db_xref"][0].split(":")[1],
+                                seq_feature.qualifiers["protein_id"][0],
+                                seq_feature.qualifiers["product"][0],
+                                description,
+                                seq_feature.qualifiers['translation'][0]))
+                    except:
+                        try:
+
+                            output_handle.write(">gi|%s|ref|%s| [%s]\n%s\n" % (
+                                seq_feature.qualifiers["db_xref"][0].split(":")[1],
+                                seq_feature.qualifiers["protein_id"][0],
+                                #seq_feature.qualifiers["note"][0],
+                                description,
+                                seq_feature.qualifiers['translation'][0]))
+                        except:
+                            output_handle.write(">gi|%s|ref|%s| [%s]\n%s\n" % (
+                                seq_feature.qualifiers["locus_tag"][0],
+                                seq_feature.qualifiers["locus_tag"][0],
+                                #seq_feature.qualifiers["note"][0],
+                                description,
+                                seq_feature.qualifiers['translation'][0]))
 
 
 if __name__ == '__main__':
@@ -58,7 +79,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", '--input_gbk', type=str, help="input gbk file")
     parser.add_argument("-o", '--outname', type=str, help="putput_name", default=False)
-
+    parser.add_argument("-f", '--format', action='store_true', help="format header: >locus description", default=False)
 
     args = parser.parse_args()
 
@@ -69,4 +90,4 @@ if __name__ == '__main__':
 
     input_handle = open(args.input_gbk, "rU")
     seq_records = list(SeqIO.parse(input_handle, "genbank"))
-    gbk2faa(seq_records, outname)
+    gbk2faa(seq_records, outname, args.format)
