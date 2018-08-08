8e193d15ecf2655d1d23b6ac86be2cbe6df8249a
tpillone <trestan.pillonel@unil.ch>
2018-08-08 14:36:50 +0200

    -

--- gbk2faa.pl
+++ gbk2faa.pl
@@ -1,134 +1,66 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+# This program reads gbk files and extracts all amino acid sequences from the
+# /translation fields into a FASTA file. The FASTA header contains a sequential
+# number followed by the taxon id, which is extracted from the
+# /db_xref="taxon:<ID>" field. Only letters in the 20 letter amino acid
+# alphabet are retained in the FASTA file.
+#
+# author: Peter Menzel
+#
+# This file is part of Kaiju, Copyright 2015-2017 Peter Menzel and Anders Krogh
+# Kaiju is licensed under the GPLv3, see the file LICENSE.
+#
 
-print "\n**********************************************************";
-print "\n*    gbk2faa.pl , CopyRight @ cail, Fudan University.    *";
-print "\n*    Convert a file in current dir please enter 1,       *";
-print "\n*    Convert all files in current dir please enter 2.    *";
-print "\n**********************************************************";
-print "\nPlease enter your choice (1 or 2) : ";
-my $choice=<STDIN>;
-print "\nGOOD LUCK!\n\n";
-chop($choice);
+use strict;
+use IO::Uncompress::AnyUncompress qw(anyuncompress $AnyUncompressError) ;
 
-if($choice==1){
-	print "Please input the ACCESSION ID of the files : ";
-	my $input=<STDIN>;
-	chop($input);
-	my $filename=$input.".gbk";
-	my $output='';
-	$output=$input.".faa";
-	gbk2faa($filename,$output);
-}
-elsif($choice==2){
-	my $dir = ".";
-	opendir(DIR0, $dir) or die "Can't open $dir: $!\n";
-	while (defined(my $filename = readdir(DIR0))) {
-		next unless $filename =~ /\.gbk$/;
-		my $output=substr($filename,0,9).".faa";
-		print "$filename -> $output\n";
-		gbk2faa($filename,$output);
+my $t = 0;
+my $taxid;
+my $protein_id;
+
+if(!defined $ARGV[1]) { die "Usage: $0 infile.gbk outfile.faa"; }
+open(OUT,">",$ARGV[1]) or die "Could not open file $ARGV[1] for writing.";
+my $in_fh = new IO::Uncompress::AnyUncompress $ARGV[0] or die "Opening input file failed: $AnyUncompressError\n";
+
+while(<$in_fh>) {
+	chomp;
+	if(m,/db_xref="taxon:(\d+)",) {
+		$taxid = $1;
 	}
-}
-else{
-	print "The program does not support this request. SORRY.";
-}
-
-exit;
-	
-sub gbk2faa{
-	
-	my ($origianl_file,$out_file)=@_;
-	my $o=''; #organism
-	my $d=''; #total sequence
-	my $d1='NONE'; #db_xref:"GI
-	my $d2='NONE'; #protein_id
-	my $d3='NONE'; #product, protein name
-	my $d4='NONE'; #amino acid sequence
-	my $d4t=''; #temp of d4
-
-	my $gbk='';
-	my @GenBankFile=();
-	my @draft=();
-
-	# Get the GenBank data into an array from a file
-	@GenBankFile=get_file_data($origianl_file);
-
-	# Extract the sequence
-	foreach my $line (@GenBankFile) {
-		if($line =~/^FEATURES/) {
-			$true=1;
+	elsif(m,/protein_id="([^"]+)",) {
+		$protein_id=$1;
+	}
+	elsif(m,\s+/translation="([^"]+)",)  {  
+		if(!defined($taxid)) { die "No taxon id found in gbk file $ARGV\n";}
+		print OUT ">$protein_id\_$taxid\n";
+		my $seq = $1;
+		$seq =~ tr/BZ/DE/;  # a.a. alphabet specifies `B' matches `N' or `D', and `Z' matches `Q' or `E.', here we use substitution with higher score
+		$seq =~ s/[^ARNDCQEGHILKMFPSTWYV]//gi;
+		print OUT "$seq\n";
+	}
+	elsif(m,\s+/translation="([^"]+)$,) {
+		if(!defined($taxid)) { die "No taxon id found in gbk file $ARGV\n";}
+		print OUT ">$protein_id\_$taxid\n";
+		$t = 1;
+		my $seq = $1;
+		$seq =~ tr/BZ/DE/;
+		$seq =~ s/[^ARNDCQEGHILKMFPSTWYV]//gi;
+		print OUT "$seq\n";
+	}
+	elsif($t) {
+		if(m,",) { 
+			tr/BZ/DE/;
+			s/[^ARNDCQEGHILKMFPSTWYV]//gi;
+			print OUT $_,"\n";   
+			$t = 0;
 		}
-		if($true) {
-			chop($line);
-			$gbk.=$line;
-			$true++;
+		else { 
+			tr/BZ/DE/;
+			s/[^ARNDCQEGHILKMFPSTWYV]//gi;
+			print OUT $_,"\n";   
 		}
 	}
-	
-	$gbk=~s/                     (\/)/\n$1/g;
-	$true=0;
-	@draft=split(/\n/,$gbk);
-	
-	foreach my $line (@draft){
-		if($line=~/^\/product="/) {
-			$line=~s/^\/product="(.*)"/$1/g;
-			$line=~s/-                     /-/g;
-			$line=~s/                     / /g;
-			$d3=$line;
-        }
-        elsif($line=~/^\/organism="/) {
-			$line=~s/^\/organism="(.*)"/$1/g;
-			$o=$line;
-        }
-		elsif($line=~/^\/protein_id="/) {
-			$line=~s/^\/protein_id="(.*)"/$1/g;
-			$d2=$line;
-        }
-		elsif($line=~/^\/db_xref="GI:/) {
-			$line=~s/^\/db_xref="GI:(.*)"/$1/g;
-			$d1=$line;
-        }
-		elsif($line=~/^\/translation="/) {
-			$line=~s/^\/translation="(.*)".*/$1/g;
-			$line=~s/\s*//g;
-			$d4t=$line;
-			$d4='';
-			my $i=0;
-			while($i<length($d4t)) {
-				$d4.=substr($d4t,$i,70)."\n";
-				$i+=70;
-			}
-			$true++;
-			if($true>0) {
-					$d=$d.">cl|".$d1."|man|".$d2."| ".$d3." [".$o."]\n".$d4;
-					$d1='NONE';#it is better to use CDS.
-					$d2='NONE';
-					$d3='NONE';
-					$d4='NONE';
-			}
-#			print "$true\n"; #number of protein found
-        }
-	}
-
-	open(FAA,">$out_file");
-	print FAA $d;
-	close FAA;
-	return 1;
 }
+close($in_fh);
+close(OUT);
 
-sub get_file_data{
-	
-	my ($file)=@_;
-	use strict;
-	use warnings;
-
-	# Initialize variables
-	my @filedata=();
-	unless(open(GET_FILE_DATA,$file)) {
-		print STDERR "Cannot open file \"$file\"\n\n";
-		exit;
-	}
-	@filedata=<GET_FILE_DATA>;
-	close GET_FILE_DATA;
-	return @filedata;
-}
