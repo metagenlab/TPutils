0bf56b1db5cd101463a4ae4ad3031f8a6727c954
Trestan Pillonel <trestan.pillonel@gmail.com>
2019-10-24 12:51:33 +0200

    test

--- setup.py
+++ setup.py
@@ -177,10 +177,10 @@ setup(
     #
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
-    scripts = ['bin/gbk2faa.py',
-               'bin/gbk2fna.py',
-               'bin/gbk2ffn.py',
-               'bin/gbk2table.py'],
+    scripts = ['TPutils/gbk2faa.py',
+               'TPutils/gbk2fna.py',
+               'TPutils/gbk2ffn.py',
+               'TPutils/gbk2table.py'],
 
     # List additional URLs that are relevant to your project as a dict.
     #
